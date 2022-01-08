b6458e59bcac01f5e8aba167112412cebd864826
Sarabbany <mdarifrabbani279349@gmail.com>
2022-01-09 00:23:45 +0600

    check

--- resources/views/pages/ended.blade.php
+++ resources/views/pages/ended.blade.php
@@ -7,10 +7,14 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('../page/ico-pages.css') }}">
+      <link rel="icon" type="image/png" href="{{ asset('images/logo/LogoDrops.png') }}" />
 
 
     @yield('customCss')
 </head>
+
+
+
 <body>
 
     @include('includes.navbar')
